472e93501142ead990d91ebf1003069441c3701b
zlu <luzhanxun.daniel@gmail.com>
2015-03-30 22:27:35 -0700

    test connection to Cassandra
    Pycharm project file
    test.py to connect to database and query data
    data.cql to create Cassandra database keyspace

--- playground/Cassandra/test.py
+++ playground/Cassandra/test.py
@@ -1,1 +1,7 @@
-print 'hello world'
+__author__ = 'Daniel'
+
+from cassandra.cluster import Cluster
+cluster = Cluster()
+session = cluster.connect("briskyprocess")
+result = session.execute("Select * from stock")
+print(result)
