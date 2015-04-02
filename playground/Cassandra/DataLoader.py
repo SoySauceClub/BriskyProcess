af7f13b2d0a89283987a3d3dc2f109205940891a
zlu <luzhanxun.daniel@gmail.com>
2015-04-01 22:34:42 -0700

    Refactor DataLoader
    Refactor DataLoader.py to make saving data function reusable

--- playground/Cassandra/DataLoader.py
+++ playground/Cassandra/DataLoader.py
@@ -4,17 +4,29 @@ import pandas
 from cassandra.cluster import Cluster
 from cassandra.query import BatchStatement
 
+def loadDataIntoDatabase(stockId, dbSession):
+    "save data to databaes"
+    query = dbSession.prepare("insert into stockdata (stock_id, time, open_price, high_price, low_price, close_price, volumne, adj_close) values(?, ?, ?, ?, ?, ?, ?, ?)")
+    batch = BatchStatement()
+    csvData = pandas.io.parsers.read_csv('Data/sampleData.csv', sep=',', skiprows=1)
+    for index, row in csvData.iterrows():
+        batch.add(
+            query, (stockId, pandas.to_datetime(row[0]), row[1], row[2], row[3], row[4], row[5], row[6])
+        )
+    dbSession.execute(batch)
+    print("save data for stock:" + stockId)
+    return
+
 cluster = Cluster()
 session = cluster.connect("briskyprocess")
-query = session.prepare("insert into stockdata (stock_id, time, open_price, high_price, low_price, close_price, volumne, adj_close) values(?, ?, ?, ?, ?, ?, ?, ?)")
-batch = BatchStatement()
-
-csvData = pandas.io.parsers.read_csv('Data/sampleData.csv', sep=',', skiprows=1)
-for index, row in csvData.iterrows():
-    batch.add(
-        query, ('AAPL', pandas.to_datetime(row[0]), row[1], row[2], row[3], row[4], row[5], row[6])
-    )
-session.execute(batch)
-
-result = session.execute("Select * from stockdata")
+#loadDataIntoDatabase("GOOG", session)
+#loadDataIntoDatabase("MSFT", session)
+#loadDataIntoDatabase("ORCL", session)
+#loadDataIntoDatabase("IBM", session)
+#loadDataIntoDatabase("SAP", session)
+#loadDataIntoDatabase("SNE", session)
+#loadDataIntoDatabase("VOXX", session)
+#loadDataIntoDatabase("ADBE", session)
+#loadDataIntoDatabase("CSCO", session)
+result = session.execute("Select count(*) from stockdata limit 200000")
 print(result)
